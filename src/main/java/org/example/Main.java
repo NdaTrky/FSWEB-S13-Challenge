dd21c47752f822ccaf9055202a3442f029102e42
nıdaturkay <145764089+NdaTrky@users.noreply.github.com>
2024-11-20 00:19:24 +0300

    proje tamamlandı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,25 +8,25 @@ public class Main {
     }
 
     public static void run() {
-        // Healthplan oluşturuluyor
+        // Healthplan
         Healthplan basicPlan = new Healthplan(1, "Basic Health Plan", Plan.BASIC);
         System.out.println(basicPlan);
 
-        // Employee oluşturuluyor (healthplans dizisi String[] tipinde olmalı)
+        // (healthplans dizisi String[] tipinde olmalı)
         String[] healthplans = new String[3];  // Sağlık planlarını tutacak dizi
         Employee employee = new Employee(101, "John Doe", "john.doe@example.com", "password123", healthplans);
 
-        // Sağlık planlarını ekliyoruz
+        // Sağlık planları
         employee.addHealthPlan(0, "Basic Health Plan");
         employee.addHealthPlan(1, "Premium Health Plan");
         employee.addHealthPlan(1, "VIP Health Plan"); // Error mesajı basılmalı
         System.out.println(employee);
 
-        // Company oluşturuluyor (developerNames dizisi String[] tipinde olmalı)
+        // Company oluştur (developerNames dizisi String[] tipinde olmalı)
         String[] developerNames = new String[5];  // Geliştiricilerin ismini tutacak dizi
         Company company = new Company(201, "TechCorp", 1_000_000.0, developerNames);
 
-        // Çalışanları ekliyoruz
+        // Çalışanlar
         company.addEmployee(0, "John Doe");
         company.addEmployee(0, "Jane Smith"); // Error mesajı basılmalı
         System.out.println(company);
